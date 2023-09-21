adfa2e4883c08bcb3544cb864d763d931a8fb0d9
Alejo Focco <alejofocco09@gmail.com>
2023-09-20 21:44:19 -0300

    El cliente puede ver su paquete con el estadon, con el lote asociado y la matricula del camion en el que va

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Paquete;
+use App\Models\Forma;
+use App\Models\LoteCamion;
+use App\Models\Camion;  
 
 class ClienteController extends Controller
 {
+    public function verPaquete(Request $request)
+    {
+        $paquete = Paquete::where('ID_Cliente', $request->id)->get();
+        $lote = Forma::whereIn('ID_Paquete', $paquete->pluck('ID'))->pluck('ID_Lote');
+        $camion = LoteCamion::whereIn('ID_Lote', $lote)->pluck('ID_Camion');
+        $matricula = Camion::whereIn('ID', $camion)->pluck('Matricula');
+        $responseArray = [
+            'paquete' => $paquete,
+            'matricula' => $matricula
+        ];
+        return response()->json(['paquete' => $responseArray], 200);
+    }
+
     
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MapaController;
 use App\Http\Controllers\ChoferController;
+use App\Http\Controllers\ClienteController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/mapa', [MapaController::class, 'getCoordenadas']);
 
-Route::post('/paquete', [ChoferController::class, 'cambiarEstadoPaquete'])->name('paquete.cambiarEstado');
+Route::post('/paquete', [ChoferController::class, 'cambiarEstadoPaquete'])->name('chofer.cambiarEstado');
 
-Route::get('/paquete', [ChoferController::class, 'verPaquetes'])->name('paquete.verPaquete');
+Route::get('/paquete', [ChoferController::class, 'verPaquetes'])->name('chofer.verPaquete');
+
+Route::get('/cliente', [ClienteController::class, 'verPaquete'])->name('cliente.verPaqueteCliente');
+
+Route::post('/chofer', [ChoferController::class, 'plataforma'])->name('chofer.plataforma');
+
+Route::post('/maneja', [ChoferController::class, 'manejaCamion'])->name('chofer.manejaCamion');
